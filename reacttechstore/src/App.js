f11ebcc38151d18c1fbc3771140930b4da44d6a4
Evon Sanders <evonsanders@Evons-MacBook-Pro.local>
2019-12-14 19:45:02 -0600

    first commit

---
+++ reacttechstore/src/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+class App extends Component {
+
+render(){
+  return <h1>hello from tech store</h1>
+}
+}
+export default App;
